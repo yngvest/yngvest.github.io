41a2ab3a2ee071f65a0a259edfc5198bac5fd6e9
Igor Shtukarev <ishtukarev@itoolabs.com>
2022-09-18 20:01:38 +0200

    [main] i hate css

--- src/horizontal.js
+++ src/horizontal.js
@@ -2,9 +2,17 @@
 
 window.onload = function() {
     let viewport = document.querySelector('html');
+    let scroll = 0;
     window.addEventListener("wheel", function (e) {
-        let delta = e.deltaY > 0 ? 100 : -100;
-        console.log(delta)
-        viewport.scrollLeft += delta;
+        let d = e.deltaY + e.deltaX;
+        scroll = clamp(scroll + d, -99999, 0)
+        viewport.style.transform = 'translateX(' + scroll + 'px)';
     });
+}
+
+function clamp(x, min, max) {
+    if(x >= min && x <= max) {
+        return x
+    }
+    return x > max ? max : min;
 }
